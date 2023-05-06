89c11c9e355c3e131de7475cc5e1eff693ddc1cf
rphl0329 <84053397+rphl0329@users.noreply.github.com>
2023-05-06 09:27:12 +0000

    correct error

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -25,5 +25,5 @@ export function setClick(selector, callback) {
 export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const product = urlParams.get('product')
+  return urlParams.get(param);
 }
