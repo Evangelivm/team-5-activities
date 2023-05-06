4939032d3fb83a116df62821051e63246af617f9
rphl0329 <84053397+rphl0329@users.noreply.github.com>
2023-05-06 09:53:45 +0000

    final edit

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,5 +1,31 @@
-import { getParam } from "./utils.mjs";
-import productDetails from "./productDetails.mjs";
+import { setLocalStorage } from './utils.js';
+import { findProductById } from "./productData.mjs";
 
-const productId = getParam("product");
-productDetails(productId);
+let product = null;
+
+//product details
+export default async function productDetails(productId){
+    product = await findProductById(productId);
+    renderProductDetails();
+    document
+    .getElementById("addToCart")
+    .addEventListener("click", addToCart);
+}
+
+//addToCart
+function addToCart(){
+    setLocalStorage("so-cart",product);
+}
+
+//renderproductdetails
+function renderProductDetails(){
+    document.querySelector("#productName").innerText = product.Brand.Name;
+    document.querySelector("#productNameWithoutBrand").innerText = product.NameWithoutBrand;
+    //for images
+    document.querySelector("#productImage").src = product.Image;
+    document.querySelector("#productImage").alt = product.Name;
+    document.querySelector("#productFinalPrice").innerText = product.FinalPrice;
+    document.querySelector("#productColorName").innerText = product.Colors(0).ColorName;
+    document.querySelector("#productDescriptionHtmlSimple").innerHTML = product.DescriptionHtmlSimple;
+    document.querySelector("#addToCart").dataset.id = product.Id;
+}
