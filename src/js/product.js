e1f0288f06f24ba27820b91bc0143bf094a806a5
Jruiz1980 <johnathanruiz@gmail.com>
2023-05-06 18:53:35 -0500

    add changes

--- src/js/product.js
+++ src/js/product.js
@@ -1,19 +1,5 @@
-import { findProductById } from "./productData.mjs";
-import { setLocalStorage, getParam } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
+import productDetails from "./productDetails.mjs";
 
 const productId = getParam("product");
-console.log(findProductById(productId));
-
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+productDetails(productId);
