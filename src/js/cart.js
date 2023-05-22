bb12c7c03030ae9581859ed21239881fa43cb59c
Johnathan Ruiz <97146171+Jruiz1980@users.noreply.github.com>
2023-05-22 14:46:07 -0500

    cart.js
    Add changes

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,29 +1,5 @@
-import { getLocalStorage } from "./utils.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+import shoppingCart from "./shoppingCart.mjs";
 
-function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
-
-  return newItem;
-}
-
-function renderCartContents() {
-  let cartItems = getLocalStorage("so-cart");
-  cartItems = [cartItems];
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
-}
-
-renderCartContents();
+loadHeaderFooter();
+shoppingCart();
