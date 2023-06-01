4ddc193d05c505f36049fa67390b99380d4efda4
jruizdev80 <johnathanruiz@gmail.com>
2023-06-01 18:45:47 -0500

    Add file changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,3 @@
-import productList from "./productList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
-import Alert from "./alert.js";
 
-
-const alertInstance = new Alert();
-alertInstance.loadAlerts();
-alertInstance.createAlertSection();
 loadHeaderFooter();
-
-productList(".product-list", "tents");
