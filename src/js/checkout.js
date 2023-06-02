0a4ec68721280942697688aa528b8ad73db84149
jruizdev80 <johnathanruiz@gmail.com>
2023-06-01 23:16:48 -0500

    add changes

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -6,17 +6,17 @@ loadHeaderFooter();
 checkoutProcess.init("so-cart", ".checkout-summary");
 
 document
-    .querySelector("#zip")
-    .addEventListener(
-        "blur",
-        checkoutProcess.calculateOrdertotal.bind(checkoutProcess)
-    );
+  .querySelector("#zip")
+  .addEventListener(
+    "blur",
+    checkoutProcess.calculateOrdertotal.bind(checkoutProcess)
+  );
 
-    // this is how it would look if we listen for the submit on the form
-    document.forms["checkout"].addEventListener("submit", (e) => {
-    e.preventDefault();
-    // e.target would contain our form in this case
-    checkoutProcess.checkout(e.target);
+// this is how it would look if we listen for the submit on the form
+document.forms["checkout"].addEventListener("submit", (e) => {
+  e.preventDefault();
+  // e.target would contain our form in this case
+  checkoutProcess.checkout(e.target);
 });
 
 // listening for click on the button
