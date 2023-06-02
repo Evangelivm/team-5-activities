25b790fbbea2cff520f46b8d071d4d8240243b65
Mouhamed jean patrick Koko <97228244+202296@users.noreply.github.com>
2023-06-02 06:39:44 +0000

    Update productData.mjs
    fix burg

--- src/js/productData.mjs
+++ src/js/productData.mjs
@@ -1,5 +1,5 @@
 let baseURL = import.meta.env.VITE_SERVER_URL
-if (baseURL === undefined) baseURL = 'http://server-nodejs.cit.byui.edu:3000/';
+baseURL = 'http://server-nodejs.cit.byui.edu:3000/';
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
