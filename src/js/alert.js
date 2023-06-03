654c3c66c2ec93aab0b50ba36b1ddb89b0a79a80
Raphael <rphl.mrnda767@gmail.com>
2023-06-03 11:41:07 +0800

    format changes

--- src/js/alert.js
+++ src/js/alert.js
@@ -4,29 +4,28 @@ export default class Alert {
   }
 
   loadAlerts() {
-      fetch('js/alerts.json')
-        .then(response => response.json())
-        .then(alertsData => {
-          if (Array.isArray(alertsData)) {
-            this.alerts = alertsData;
-            this.createAlertSection();
-          }
-        })
-        .catch(error => {
-          console.error('Failed to load alerts:', error);
-        });
-    }
-    
+    fetch("js/alerts.json")
+      .then((response) => response.json())
+      .then((alertsData) => {
+        if (Array.isArray(alertsData)) {
+          this.alerts = alertsData;
+          this.createAlertSection();
+        }
+      })
+      .catch((error) => {
+        console.error("Failed to load alerts:", error);
+      });
+  }
 
   createAlertSection() {
-    const mainElement = document.querySelector('main');
+    const mainElement = document.querySelector("main");
 
     if (this.alerts.length > 0) {
-      const alertSection = document.createElement('section');
-      alertSection.classList.add('alert-list');
+      const alertSection = document.createElement("section");
+      alertSection.classList.add("alert-list");
 
-      this.alerts.forEach(alertData => {
-        const alert = document.createElement('p');
+      this.alerts.forEach((alertData) => {
+        const alert = document.createElement("p");
         alert.textContent = alertData.message;
         alert.style.background = alertData.background;
         alert.style.color = alertData.color;
