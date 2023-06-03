58d50ed0fdcb1370cea76ba343bbf00b79876577
Raphael <rphl.mrnda767@gmail.com>
2023-06-03 11:41:50 +0800

    format changes

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,6 @@ import productList from "./productList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 import Alert from "./alert.js";
 
-
 const alertInstance = new Alert();
 alertInstance.loadAlerts();
 alertInstance.createAlertSection();
