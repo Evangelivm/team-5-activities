01573c3b6a9a0279c279667ba30ff6c9450c3778
Devy Salomon <devysalomon@gmail.com>
2023-06-12 18:19:59 -0500

    new

--- src/js/login.js
+++ src/js/login.js
@@ -1,11 +1,12 @@
 import { login } from "./auth.mjs";
-import { getParam, loadHeaderFooter } from "./utils.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 
 loadHeaderFooter();
 const redirect = getParam("redirect");
 
-document.querySelector("#loginButton").addEventListener("click", (e) => {
-  const email = document.querySelector("#email").value;
-  const password = document.querySelector("#password").value;
-  login({ email, password }, redirect);
-});
+document.querySelector("#loginbutton").addEventListener("click", (e) => {
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+    login({email,password}, redirect)
+})
