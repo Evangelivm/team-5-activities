74f38b5f2cbdcd18103e7672a68191a771a4c7ce
Raphael <rphl.mrnda767@gmail.com>
2023-06-17 20:11:52 +0800

    some changes

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -14,10 +14,10 @@ document
 // this is how it would look if we listen for the submit on the form
 //document.forms["checkout"].addEventListener("submit", (e) => {
 //  e.preventDefault();
-  // e.target would contain our form in this case
- // checkoutProcess.checkout(e.target);
+// e.target would contain our form in this case
+// checkoutProcess.checkout(e.target);
 //});
 document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
-   e.preventDefault();
-   checkoutProcess.checkout(document.forms['checkout']);
- });
+  e.preventDefault();
+  checkoutProcess.checkout(document.forms["checkout"]);
+});
